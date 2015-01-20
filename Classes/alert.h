2fadb5cb621111ef2ff908c64bb61dbec8d3c7d4
Bryce Campbell <tonyhawk2100@gmail.com>
2015-01-20 10:20:26 -0800

    add files needed for alert dialog.

---
+++ Classes/alert.h
@@ -0,0 +1,13 @@
+//
+//  alert.h
+//  RMD Calculator
+//
+//  Created by Bryce Campbell on 1/20/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface alert : NSObject
+
+@end
---
+++ Classes/alert.m
@@ -0,0 +1,13 @@
+//
+//  alert.m
+//  RMD Calculator
+//
+//  Created by Bryce Campbell on 1/20/15.
+//
+//
+
+#import "alert.h"
+
+@implementation alert
+
+@end
