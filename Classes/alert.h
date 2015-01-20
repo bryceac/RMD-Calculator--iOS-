adc2fd273f35c35d5769fffd3f60c60d797d40b2
Bryce Campbell <tonyhawk2100@gmail.com>
2015-01-20 10:37:54 -0800

    implement code needed for class.

--- Classes/alert.h
+++ Classes/alert.h
@@ -8,6 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
-@interface alert : NSObject
+@interface alert : NSObject <UIAlertViewDelegate>
+{
+    NSString *title, *message;
+    UIAlertView *alert;
+}
 
+// set up properties, in order to assign variables easily
+@property (nonatomic, retain) NSString *title;
+@property (nonatomic, retain) NSString *message;
+
+// create methods needed for class
+- (void)createDialog:(NSString*)t message:(NSString*)m question:(BOOL*)b; // method to create dialog
+- (void)show; // method to show dialog
+
+- (id) initWithTitle:(NSString*)t message:(NSString *)m andType:(Bool*)b; // initializer to quickly create dialogs
 @end
--- Classes/alert.m
+++ Classes/alert.m
@@ -9,5 +9,26 @@
 #import "alert.h"
 
 @implementation alert
+@synthesize title, message;
+
+- (id) initWithTitle:(NSString *)t message:(NSString *)m andType:(id)b
+{
+    if (self = [super init])
+    {
+        self.title = t;
+        self.message = m;
+    }
+    return self;
+}
+
+- (void)createDialog:(NSString *)t message:(NSString *)m question:(BOOL *)b
+{
+    
+}
+
+- (void)show
+{
+    
+}
 
 @end
